bc207b6d4158bb12bcca1e189a16142ed4185534
imtiajul-islam-d <imtiajul@hotmail.com>
2023-10-19 11:17:18 +0600

    day one completed

---
+++ Day_one/show_full_name.py
@@ -0,0 +1,5 @@
+print("----------------See your fullname.---------------------")
+first_name = input("What is your first_name? ")
+last_name = input("What is your last_name? ")
+print(first_name,last_name)
+
